b8f07641be4bc2a064160883185f61c32e6b42ba
rafasoyyo <rafa_soyyo@hotmail.com>
2018-10-16 08:13:16 +0200

    Base Project

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+
+var express = require('express');
+var router = express.Router();
+
+var places = require('./places');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET maps page. */
+router.get('/sitios/:sitio', function(req, res, next) {
+  var item = places[req.params.sitio];
+  res.render('maps', {
+    title: item.title,
+    places: item.places
+  });
+});
+
+
+module.exports = router;
