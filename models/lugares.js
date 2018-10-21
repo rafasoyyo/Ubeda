2e2342e311cd4d7778bb939c6146db54d430572a
rafasoyyo <rafa_soyyo@hotmail.com>
2018-10-21 17:19:14 +0200

    data base added

--- models/lugares.js
+++ models/lugares.js
@@ -5,10 +5,13 @@ var lugarSchema = new Schema({
     "id"  : { type: 'String' },
     "name": { type: 'String' },
     "type": { type: 'String' },
-    "info": { type: 'String' }
+    "info": { type: 'String' },
+    "recomendation": { type: 'String' }
 });
 
-module.exports = mongoose.model('LosLugares', lugarSchema);
-module.exports = mongoose.model('Turisteo', lugarSchema);
-module.exports = mongoose.model('padormir', lugarSchema);
-module.exports = mongoose.model('detapas', lugarSchema);
+module.exports = {
+    LosLugares : mongoose.model('LosLugares', lugarSchema),
+    PaDormir : mongoose.model('padormir', lugarSchema),
+    DeTuristeo : mongoose.model('Turisteo', lugarSchema),
+    DeTapas : mongoose.model('detapas', lugarSchema)
+}
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,37 @@
 var express = require('express');
 var router = express.Router();
 
-var places = require('./places');
+var Lugares = require('../models/lugares.js')
+var LosLugares = Lugares.LosLugares;
+var DeTuristeo = Lugares.DeTuristeo;
+var PaDormir = Lugares.PaDormir;
+var DeTapas = Lugares.DeTapas;
+// console.log('Lugares -> ', Lugares);
+// var places = require('./places');
+
+// Object.keys(places).forEach(function(element) {
+//   (places[element].places).forEach(function(place){
+//     console.log('Lugares[element] -> ', Lugares[element]);
+
+//     Lugares[element].create(place)
+//   });
+// });
+
+
+var places = {
+    LosLugares: {
+        title: "Lugares de la boda",
+    },
+    PaDormir: {
+        title: "Alojamientos",
+    },
+    DeTuristeo: {
+        title: "Turismo",
+    },
+    DeTapas: {
+        title: "Tapeo en Úbeda",
+    }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,12 +42,31 @@ router.get('/', function(req, res, next) {
 
 /* GET maps page. */
 router.get('/sitios/:sitio', function(req, res, next) {
-  var item = places[req.params.sitio];
-  res.render('maps', {
-    title: item.title,
-    places: item.places
+
+  Lugares[req.params.sitio].find(function(err, results) {
+    console.log('results -> ', results);
+    if (err) {
+      res.render('error', {
+        err: { status: err.id, stack: err.toString }
+      });
+    } else {
+      res.render('maps', {
+          title: places[req.params.sitio].title,
+          places: results
+      });
+    }
   });
+
 });
 
+/* GET maps page. */
+router.get('/nuevoinvitado', function(req, res, next) {
+  res.render('nuevo_invitado', { title: 'Invitados' });
+});
 
+/* GET maps page. */
+router.post('/nuevoinvitado', function(req, res, next) {
+
+
+});
 module.exports = router;
